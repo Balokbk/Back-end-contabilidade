c76483963db0cc70eb184a823cfd2d62c2b38015
Balo <kawanbalonecker@gmail.com>
2024-10-28 21:54:12 -0300

    erro no CORS

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const routes = require('./routes')
 const cors = require('cors')
 
 const app = express()
-app.use(cors)
+app.use(cors({
+    methods:['GET', 'POST', 'PUT', 'DELETE']
+}))
 app.use(express.json())
 
 routes(app)
